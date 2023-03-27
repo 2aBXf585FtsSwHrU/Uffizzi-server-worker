1ab385ffc9f3ffa96b1b8a9cbb56ee184ac84f20
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-03-27 21:03:49 +0700

    update random

--- random.sh
+++ random.sh
@@ -7,7 +7,7 @@ do
   response=$(curl -s -o /dev/null -w "%{http_code}" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0" "https://mirror.touhidur.studio/$predictableUrl")
   if [ $response -eq 200 ]
   then
-    echo "\n"
+    echo -e "\n"
   else
     echo "访问失败，响应码为 $response"
   fi
