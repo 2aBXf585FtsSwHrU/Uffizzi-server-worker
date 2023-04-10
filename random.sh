c3c9e5baa4f4eee64cb38d1f8b8ccb40389580da
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-04-10 17:11:39 +0700

    fix error

--- random.sh
+++ random.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 while true
 do
   predictableUrl=${UFFIZZI_PREDICTABLE_URL#*://} # 去掉 http:// 部分
   echo "URL https://$predictableUrl"
-  response=$(curl -s -o /dev/null -w "%{http_code}" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0" "https://mirror.touhidur.studio/$predictableUrl")
+  response=$(curl -s -o /dev/null -w "%{http_code}" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0" --socks5 127.0.0.1:21080 "https://$predictableUrl")
   if [ $response -eq 200 ]
   then
     echo -e "\n"
